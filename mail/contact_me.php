3cff4230934306cd367eb780d79dc54efb2a71fb
lhf-leo <hanfuliao@gmail.com>
2015-10-27 22:42:22 -0400

    Site updated at 2015-10-28 02:42:22 UTC

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -22,5 +22,8 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
+mail('hanfuliao@gmail.com',$email_subject,$email_body,$headers);
+mail('liaohanfu@icloud.com',$email_subject,$email_body,$headers);
+mail('hanfuliao@126.com',$email_body,$headers);
 return true;			
 ?>
