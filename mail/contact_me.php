b9c75a258c43d95ab518c9ab3a7ada09585cb071
lhf-leo <hanfuliao@gmail.com>
2015-10-27 22:55:21 -0400

    Site updated at 2015-10-28 02:55:21 UTC

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,14 +16,11 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'liaohanfu@foxmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'hanfuliao@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Blog Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: notice@hanfu.space\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-mail('hanfuliao@gmail.com',$email_subject,$email_body,$headers);
-mail('liaohanfu@icloud.com',$email_subject,$email_body,$headers);
-mail('hanfuliao@126.com',$email_body,$headers);
 return true;			
 ?>
